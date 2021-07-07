a47787d3abee7bd764fed5c48d979a1f087567d9
lazyduo <dadanuna@gmail.com>
2021-07-08 00:04:41 +0900

    draw bitmap

--- flaskapp/static/js/app.js
+++ flaskapp/static/js/app.js
@@ -1,9 +1,23 @@
-var myFunction = function() {
-    console.log('hi');//
-};
+document.addEventListener("DOMContentLoaded", function(){
 
-var className = document.getElementsByClassName("pixel");
+    var mouseDown = 0;
+    document.body.onmousedown = function() { 
+        ++mouseDown;
+    }
+    document.body.onmouseup = function() {
+        --mouseDown;
+    }
 
-for (var i = 0; i < className.length; i++) {
-    className[i].addEventListener('click', myFunction, false);
-}
+    // Handler when the DOM is fully loaded
+    var className = document.getElementsByClassName("pixel");
+    
+    var myFunction = function() {
+        if (mouseDown)
+            this.classList.toggle("clicked");
+    };
+
+    for (var i = 0; i < className.length; i++) {
+        className[i].addEventListener('mouseover', myFunction, false);
+    }
+});
+
