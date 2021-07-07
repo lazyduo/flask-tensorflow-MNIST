981ca10d0dc0cabbca2e6f0eabec48a747269aba
lazyduo <dadanuna@gmail.com>
2021-07-07 22:37:36 +0900

    link template inheritance

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello World'
+    return render_template('index.html')
+
+@app.route('/classify')
+def classify():
+    return render_template('tensorflow/classify.html')
