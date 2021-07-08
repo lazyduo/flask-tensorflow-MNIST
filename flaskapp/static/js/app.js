d60c0f828e30de0e3446b67a47b1416c0eceb906
lazyduo <dadanuna@gmail.com>
2021-07-08 11:06:35 +0900

    add clear btn

--- flaskapp/static/js/app.js
+++ flaskapp/static/js/app.js
@@ -10,14 +10,27 @@ document.addEventListener("DOMContentLoaded", function(){
 
     // Handler when the DOM is fully loaded
     var className = document.getElementsByClassName("pixel");
+    var clear_btn = document.getElementById("clear_btn")
+
+    console.log(clear_btn)
     
     var myFunction = function() {
-        if (mouseDown)
-            this.classList.toggle("clicked");
+        if (mouseDown) {
+            this.classList.add("clicked");
+        }
     };
 
     for (var i = 0; i < className.length; i++) {
         className[i].addEventListener('mouseover', myFunction, false);
     }
+
+
+    var clear_map = function() {
+        for (var i = 0; i < className.length; i++) {
+            className[i].classList.remove("clicked");
+        }
+    }
+
+    clear_btn.addEventListener('click', clear_map, false);
 });
 
